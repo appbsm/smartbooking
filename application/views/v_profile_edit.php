13122f614c1f9cf6de925ffd37db3a6e9982b5c9
D:\BK-AA\brokerapp\broker-2 <D:\BK-AA\brokerapp\broker-2>
2024-07-12 11:59:41 +0700

    no message

--- application/views/v_profile_edit.php
+++ application/views/v_profile_edit.php
@@ -8,6 +8,12 @@ $CI->load->model('m_room_type');
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap.min.css">
 <style>
+	.form-control, label, a {
+		color: #000 !important;
+	}
+	a:hover {
+		color: #000 !important;
+	}
 	.nav-link.active {
 		background-color: #81BB4A!important;
 	}
