3582775398a6b9ddb4ef272ec16eee49c8b1cbd3
D:\BK-AA\brokerapp\broker-2 <D:\BK-AA\brokerapp\broker-2>
2024-07-23 10:44:43 +0700

    no message

--- assets/cookie/cc-init-en.js
+++ assets/cookie/cc-init-en.js
@@ -1,8 +1,37 @@
-//const config = {}
+// Adding custom CSS to the document
+const style = document.createElement('style');
+style.innerHTML = `
+	button, .btn {
+		background-color: #102958 !important;
+		color: #fff !important; /* Optional: to ensure text color is white */
+	}
+	#s-c-bn:after, #s-c-bn:before {
+		background: #444d53;
+		background: #ffffff;
+		border-radius: 1em;
+		height: .6em;
+		left: .82em;
+		top: .58em;
+		width: 1.5px;
+	}
+	.cc_div .act .b-bn .exp:before, .cc_div .b-bn .exp:before {
+		border: solid #2d4156;
+		border-color: #ffffff;
+		border-width: 0 2px 2px 0;
+		content: "";
+		display: inline-block;
+		left: 1.2em;
+		margin-right: 15px;
+		padding: .2em;
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%) rotate(45deg);
+	}
+`;
+document.head.appendChild(style);
 
-// let cc = initCookieConsent();
-// cc.run(config);
- let cc = initCookieConsent();
+// Initialize cookie consent
+let cc = initCookieConsent();
 const config = {
   current_lang: "th",
   autorun: true,
@@ -10,23 +39,23 @@ const config = {
   languages: {
     en: {
       consent_modal: {
-        title: '<p style="font-size:16px;font-family:manrope;">This website uses cookies</p>',
+        title: '<p style="font-size:16px; color: #000;">This website uses cookies</p>',
         description:
-          '<p style="font-family:manrope;">this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Cookie settings</button></p>',
+          '<p style="color: #000; ">This website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link" style="background-color: #fff !important; color: #000 !important;">Cookie settings</button></p>',
         primary_btn: {
-          text: '<p style="font-family:manrope;text-align:center;">Accept</p>',
+          text: '<p style="font-family:manrope;text-align:center; color: #fff;">Accept</p>',
           role: "accept_all", // 'accept_selected' or 'accept_all'
         },
         secondary_btn: {
-          text: '<p style="font-family:manrope;text-align:center;">Reject</p>',
+          text: '<p style="text-align:center; color: #fff;">Reject</p>',
           role: "accept_necessary", // 'settings' or 'accept_necessary'
         },
       },
       settings_modal: {
-        title: '<p style="font-size:20px;font-family:manrope;text-align:left;">Cookie settings</p>',
-		save_settings_btn: '<p style="font-family:manrope;text-align:center;">Save settings</p>',
-        accept_all_btn: '<p style="font-family:manrope;text-align:center;">Accept all</p>',
-        reject_all_btn: '<p style="font-family:manrope;text-align:center;">Reject all</p>',
+        title: '<p style="font-size:20px; color: #000; text-align:left;">Cookie settings</p>',
+        save_settings_btn: '<p style="text-align:center; background-color: #102958; color: #fff;">Save settings</p>',
+        accept_all_btn: '<p style="text-align:center; background-color: #102958; color: #fff;">Accept all</p>',
+        reject_all_btn: '<p style="text-align:center; background-color: #102958; color: #fff;">Reject all</p>',
         close_btn_label: "Close",
         cookie_table_headers: [
           { col1: "Name" },
@@ -36,14 +65,14 @@ const config = {
         ],
         blocks: [
           {
-            title: '<p style="font-size:18px;font-family:manrope;text-align:left;">Necessary cookies 📢</p>',
+            title: '<p style="font-size:18px; color: #000; text-align:left;">Necessary cookies 📢</p>',
             description:
-              '<p style="font-family:manrope;text-align:left;">Cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="../en/cookie-policy.php" class="cc-link">cookie policy</a>.</p>',
+              '<p style="color: #000; text-align:left;">Cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="https://smartbooking.installdirect.asia/Cookiepolicy" class="cc-link">cookie policy</a>.</p>',
           },
           {
-            title: '<p style="font-family:manrope;text-align:left;">Strictly necessary cookies</p>',
+            title: '<p style="text-align:left;">Strictly necessary cookies</p>',
             description:
-              '<p style="font-family:manrope;text-align:left;">These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly</p>',
+              '<p style="color: #000; text-align:left;">These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly</p>',
             toggle: {
               value: "necessary",
               enabled: true,
@@ -51,20 +80,19 @@ const config = {
             },
           },
           {
-            title: '<p style="font-family:manrope;text-align:left;">Cookies for analysis and evaluation</p>',
+            title: '<p style="text-align:left;">Cookies for analysis and evaluation</p>',
             description:
-              '<p style="font-family:manrope;text-align:left;">Performance cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They enable us to understand how the Viewer/User interact with the Website by collecting and reporting information anonymously and to help us improve user experience of the Website.</p>',
+              '<p style="color: #000; text-align:left;">Performance cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They enable us to understand how the Viewer/User interact with the Website by collecting and reporting information anonymously and to help us improve user experience of the Website.</p>',
             toggle: {
               value: "analytics", // your cookie category
               enabled: false,
               readonly: false,
             },
-            
           },
           {
-            title: '<p style="font-family:manrope;text-align:left;">Advertisement and Targeting cookies</p>',
+            title: '<p style="text-align:left;">Advertisement and Targeting cookies</p>',
             description:
-              '<p style="font-family:manrope;text-align:left;">This type of cookie stores information about your visit to the website. What websites do you visit? And visit the website through which links?</p>',
+              '<p style="color: #000; text-align:left;">This type of cookie stores information about your visit to the website. What websites do you visit? And visit the website through which links?</p>',
             toggle: {
               value: "targeting",
               enabled: false,
@@ -77,4 +105,4 @@ const config = {
   },
 };
 
- cc.run(config);
+cc.run(config);
